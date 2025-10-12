b3144c41a277b5c5cec5e741c5b33d08bdda19b2
BalinValery <valera.balin00@mail.ru>
2025-10-12 19:12:23 +0500

    Task1

---
+++ Tyuiu.BalinVV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BalinVV.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Валерий";
+            int age = 21;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " год!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
