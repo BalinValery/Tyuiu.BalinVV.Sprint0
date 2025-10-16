c00a231dcc1106d40b800f4f60c0b84a84478741
BalinValery <valera.balin00@mail.ru>
2025-10-17 00:03:33 +0500

    Task7

---
+++ Tyuiu.BalinVV.Sprint0.Task7.V0.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BalinVV.Sprint0.Task7.V0.Lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resualtArray = new int[5];
+            for (var i = 0; i < numOne.Length; i++)
+            {
+                resualtArray[i] = numOne[i] + numTwo[i];
+            }
+            return resualtArray;
+        }
+    }
+}
---
+++ Tyuiu.BalinVV.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BalinVV.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BalinVV.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1d309caa-78e9-4e90-8669-b592d5feb081")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
