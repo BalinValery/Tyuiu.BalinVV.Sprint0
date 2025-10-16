ee79f817ac91630888db5da4ea5a1bc1fb6b6bfb
BalinValery <valera.balin00@mail.ru>
2025-10-16 18:12:28 +0500

    Task2

---
+++ Tyuiu.BalinVV.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Tyuiu.BalinVV.Sprint0.Task2.V0.Lib; 
+
+namespace Tyuiu.BalinVV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Валерий";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Валерий",res);
+        }
+    }
+}
